6e40d997bdb72aa886922df5d72b2ca94d6e943e
prosvetovvv <vp12@yandex.ru>
2020-09-22 13:25:47 +0300

    second commit

---
+++ VKCloneWithoutStoryboard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VKCloneWithoutStoryboard
+//
+//  Created by Vitaly Prosvetov on 22.09.2020.
+//  Copyright © 2020 Vitaly Prosvetov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
